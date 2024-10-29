7ca3837f27751acf10040565988fdb48d766e31d
sibelly <sibellychan@hotmail.com>
2024-10-29 14:16:24 -0400

    fix: substrate cfg send

--- go/substrate.go
+++ go/substrate.go
@@ -1,30 +1,28 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 
 	gsrpc "github.com/centrifuge/go-substrate-rpc-client/v4"
-	"github.com/centrifuge/go-substrate-rpc-client/v4/scale"
 	"github.com/centrifuge/go-substrate-rpc-client/v4/signature"
 	"github.com/centrifuge/go-substrate-rpc-client/v4/types"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types/codec"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types/extrinsic"
+	"github.com/centrifuge/go-substrate-rpc-client/v4/types/extrinsic/extensions"
 )
 
 func main() {
 	err := SendData()
-
 	if err != nil {
 		fmt.Println("error:", err)
-
 	}
-
 }
 
 func SendData() error {
-	//kos private key
+	//mnemonic
 	memo := ""
 
-	api, err := gsrpc.NewSubstrateAPI("")
+	api, err := gsrpc.NewSubstrateAPI("WSS ADDRESS HERE")
 	if err != nil {
 		return err
 	}
@@ -40,30 +38,21 @@ func SendData() error {
 		return err
 	}
 
-	amount := types.NewUCompactFromUInt(1)
+	amount := types.NewUCompactFromUInt(1000000)
 
-	bob, err := types.NewMultiAddressFromHexAccountID("")
+	ADDR2, err := types.NewMultiAddressFromHexAccountID("OTHER ADDRESS")
 	if err != nil {
 		return err
 	}
 
-	c, err := types.NewCall(meta, "Balances.transfer_keep_alive", bob, amount)
+	c, err := types.NewCall(meta, "Balances.transfer_keep_alive", ADDR2, amount)
 	if err != nil {
 		fmt.Println("err call", err)
 		return err
 	}
 	// Create the extrinsic
-	ext := types.NewExtrinsic(c)
+	ext := extrinsic.NewExtrinsic(c)
 	fmt.Println("extrinsic version:", ext.Version)
-	fmt.Println("extrinsic:", ext)
-
-	// Assuming `extrinsicBytes` is the byte slice of your encoded extrinsic
-	var seilah types.Extrinsic
-	extrinsicBytes := []byte("0x4d0284006a298320c2b3422006b7896b2f102ff7fad99f26a0d9807642a293667cdac13e0100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000140300dc0c382e18e8765f85170c5f3fb9158b9628c71442d3394f512845ac50a341441700b813d4d5a39e2702")
-	decoder := scale.NewDecoder(bytes.NewReader(extrinsicBytes))
-	seilah.Decode(*decoder)
-
-	fmt.Println("seilah ", seilah)
 
 	genesisHash, err := api.RPC.Chain.GetBlockHash(0)
 	if err != nil {
@@ -76,41 +65,47 @@ func SendData() error {
 		fmt.Println("GetRuntimeVersionLatest, err", err)
 		return err
 	}
-	fmt.Println("done 3")
+	fmt.Println("Runtime version => ", rv)
 	key, err := types.CreateStorageKey(meta, "System", "Account", keypair.PublicKey)
 	if err != nil {
 		return err
 	}
-	fmt.Println("done 4 key ", key)
+	fmt.Println("Key ", key)
 	var accountInfo types.AccountInfo
 	ok, err := api.RPC.State.GetStorageLatest(key, &accountInfo)
-	fmt.Println("done 5", ok, err, accountInfo)
+	fmt.Println("accountInfo => ", ok, err, accountInfo)
 	if err != nil || !ok {
 		return err
 	}
 
-	nonce := uint32(accountInfo.Nonce)
-	o := types.SignatureOptions{
-		BlockHash:          genesisHash,
-		Era:                types.ExtrinsicEra{IsMortalEra: false},
-		GenesisHash:        genesisHash,
-		Nonce:              types.NewUCompactFromUInt(uint64(nonce)),
-		SpecVersion:        rv.SpecVersion,
-		Tip:                types.NewUCompactFromUInt(100),
-		TransactionVersion: rv.TransactionVersion,
-	}
-	fmt.Println("before sign:", o)
-	// Sign the transaction using Alice's default account
-	err = ext.Sign(keypair, o)
+	// Sign the transaction using ADDR1's default account
+	err = ext.Sign(
+		keypair,
+		meta,
+		extrinsic.WithEra(types.ExtrinsicEra{IsImmortalEra: true}, genesisHash),
+		extrinsic.WithNonce(types.NewUCompactFromUInt(uint64(accountInfo.Nonce))),
+		extrinsic.WithTip(types.NewUCompactFromUInt(0)),
+		extrinsic.WithSpecVersion(rv.SpecVersion),
+		extrinsic.WithTransactionVersion(rv.TransactionVersion),
+		extrinsic.WithGenesisHash(genesisHash),
+		extrinsic.WithMetadataMode(extensions.CheckMetadataModeDisabled, extensions.CheckMetadataHash{Hash: types.NewEmptyOption[types.H256]()}),
+		extrinsic.WithAssetID(types.NewEmptyOption[types.AssetID]()),
+	)
+
 	if err != nil {
 		return err
 	}
 	fmt.Println("before submission")
-	// Send the extrinsic
-	txnHash, err := api.RPC.Author.SubmitAndWatchExtrinsic(ext)
+	encodedExt, err := codec.EncodeToHex(ext)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Ext - %s\n", encodedExt)
+
+	sub, err := api.RPC.Author.SubmitExtrinsic(ext)
 	if err != nil {
 		return err
 	}
-	fmt.Println("after submission txnHash", txnHash)
+	fmt.Println("after submission txnHash", sub.Hex())
 	return nil
 }
